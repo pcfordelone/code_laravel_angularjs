b320f17c8d714985e62322e4bfe6c83223e94913
PC Fordelone <pc@fordelone.com.br>
2016-04-22 10:56:49 -0300

    Inclusão de Model Client, sub-pastas repositories, services e requests, migration

---
+++ app/Models/Client/Client.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace FRD\Models\Client;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    //
+}
---
+++ database/migrations/2016_04_22_135111_create_clients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('type');
+            $table->string('name', 255);
+            $table->string('responsible', 100);
+            $table->string('email', 100)->unique();
+            $table->string('phone', 15);
+            $table->string('adress');
+            $table->text('obs');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('clients');
+    }
+}
