70672f84f6d07c48d6281f6cef743baf10c6a37a
PC Fordelone <pc@fordelone.com.br>
2016-04-27 16:47:07 -0300

    Criação ClientService

---
+++ app/Services/ClientService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace FRD\Services;
+
+
+use FRD\Repositories\ClientRepositoryInterface;
+
+class ClientService
+{
+    private $clientRepository;
+
+    public function __construct(ClientRepositoryInterface $clientRepository)
+    {
+        $this->clientRepository = $clientRepository;
+    }
+
+    public function store(array $data)
+    {
+        // enviar e-mail
+        // disparar notificação
+        // postar tweet
+
+        return $this->clientRepository->create($data);
+    }
+
+    public function update(array $data, $id)
+    {
+        // enviar e-mail
+        // disparar notificação
+        // postar tweet
+        
+        return $this->clientRepository->update($data, $id);
+    }
+}
