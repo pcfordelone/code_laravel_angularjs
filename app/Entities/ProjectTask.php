1a1650fdf40a1566712662fbd44554d10a3c4524
PC Fordelone <pc@fordelone.com.br>
2016-04-30 14:41:27 -0300

    criação model e migration ProjectTask

---
+++ app/Entities/ProjectTask.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace FRD\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProjectTask extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'start_date',
+        'duo_date',
+        'project_id',
+    ];
+}
