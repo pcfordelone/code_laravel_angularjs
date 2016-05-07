f54f8246df6fee151845900d04790b8809a6ae2a
PC Fordelone <pc@fordelone.com.br>
2016-05-07 00:10:30 -0300

    instalação do oauth2

--- config/app.php
+++ config/app.php
@@ -141,6 +141,8 @@ return [
          * Complements...
          */
         Prettus\Repository\Providers\RepositoryServiceProvider::class,
+        LucaDegasperi\OAuth2Server\Storage\FluentStorageServiceProvider::class,
+        LucaDegasperi\OAuth2Server\OAuth2ServerServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -201,6 +203,10 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
 
+        /*
+         * Complements...
+         */
+        'Authorizer' => LucaDegasperi\OAuth2Server\Facades\Authorizer::class,
     ],
 
 ];
